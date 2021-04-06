280518095001613a8335f873c7015c7840f54411
Ahmed Hadjou <thehidjou@yahoo.com>
2021-04-06 19:08:09 +0100

    posts modifications

--- routes/api/posts/posts.js
+++ routes/api/posts/posts.js
@@ -1,11 +1,75 @@
 'use strict'
 
+let currentId = 2
+const posts = [
+  {
+    id: 1,
+    user: 'John Doe',
+    title: 'Hello World',
+  },
+]
+
+const postOptions = {
+  schema: {
+    body: {
+      type: 'object',
+      required: ['user', 'title'],
+      properties: {
+        title: { type: 'string' },
+        user: { type: 'string' },
+      },
+    },
+    response: {
+      201: {
+        type: 'object',
+        properties: {
+          title: { type: 'string' },
+          user: { type: 'string' },
+        },
+      },
+    },
+  },
+}
+
 /**
+ *
  * @param {import('fastify').FastifyInstance} fastify
  */
 module.exports = async function (fastify, opts) {
+  const createError = fastify.httpErrors.createError
+  // Read
   fastify.get('/', async function (request, reply) {
-    throw new Error('Some Error')
-    reply.code(200).send({ posts: [] })
+    reply.status(200).send(posts)
+  })
+
+  // Create
+  fastify.post('/', postOptions, async function (request, reply) {
+    const { title, user } = request.body
+
+    const newPost = { id: currentId, title, user }
+
+    posts.push(newPost)
+
+    currentId++
+
+    reply.code(201)
+    return newPost
+  })
+
+  // Read
+  fastify.get('/:id', async function (request, reply) {
+    try {
+      const post = posts.find((p) => p.id === +request.params.id)
+
+      if (!post) {
+        return createError(404, 'This post does not exist!')
+      }
+
+      post.support = fastify.someSupport()
+
+      return post
+    } catch (err) {
+      throw new Error('Something went wrong')
+    }
   })
 }
